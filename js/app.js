fa9173011ac4c304091a958a7642b111184ed49b
sebastian <qualitycolombia.dvp03@gmail.com>
2024-05-16 16:45:49 -0500

    consulta de cotizacion de criptos, alertas y spinner, proyecto finalizado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
 const monedaSelect = document.querySelector('#moneda');
 
 const objBusqueda = {
@@ -54,13 +55,95 @@ function submitFormulario(e) {
     //Validar selects
     const { moneda, criptomoneda } = objBusqueda;
 
-    if(moneda === '' || criptomoneda === '') {
+    if (moneda === '' || criptomoneda === '') {
         mostrarAlerta('Ambos campos son obligatorios');
         return;
     }
 
+    // Consultar api con los resultados e información
+    consultarAPI();
+
 }
 
 function mostrarAlerta(mensaje) {
-    console.log(mensaje);
+
+    const alertaExiste = document.querySelector('.error');
+
+    if (!alertaExiste) {
+
+        const divAlerta = document.createElement('div');
+        divAlerta.classList.add('error');
+
+        //mensaje de alerta
+        divAlerta.textContent = mensaje;
+
+        formulario.appendChild(divAlerta);
+
+        setTimeout(() => {
+            divAlerta.remove();
+        }, 3000);
+
+    }
+
+}
+
+function consultarAPI() {
+
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]));
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+
+    limpiarHTML();
+
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    createHTMLContent(`El precio es: <span>${PRICE}</span>`, 'precio'); //precio actual
+    createHTMLContent(`<p>Precio más alto del día: <span>${HIGHDAY}</span></p>`, ''); //precio mas alto del día
+    createHTMLContent(`<p>Precio más bajo del día: <span>${LOWDAY}</span></p>`, ''); //precio mas bajo del día
+    createHTMLContent(`<p>Variación últimas 24 horas: <span>${CHANGEPCT24HOUR} %</span></p>`, ''); //Variacion del precio durante el día
+    createHTMLContent(`<p>Última actualización: <span>${LASTUPDATE}</span></p>`, ''); //Momento de ultima actualización del día
+
+}
+
+function createHTMLContent(mensaje, clase) {
+
+    const element = document.createElement('P');
+
+    if (clase !== '') {
+        element.classList.add(clase);
+    }
+    element.innerHTML = mensaje;
+
+    resultado.appendChild(element);
+}
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `;
+
+    resultado.appendChild(spinner);
+
 }
